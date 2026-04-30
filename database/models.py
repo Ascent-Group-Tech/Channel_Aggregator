4cf46a3b84bffff346fadfc2469c79b3f50c8dd4
Stanislaw12 <stasprudyvus8@gmail.com>
2026-04-30 20:12:12 +0200

    Створив структуру таблиць та підключив її до нашої БД за допомогою SQLalchemy

---
+++ database/models.py
@@ -0,0 +1,66 @@
+from sqlalchemy import String, Text, Boolean, DateTime, Integer, ForeignKey, UniqueConstraint, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from .database import Base
+
+#Таблиця де кожному донорському каналу присвоюється ункальний ID
+class Channels(Base):
+    __tablename__ = "channels"
+    
+    channel_id: Mapped[int] = mapped_column(primary_key=True)
+    channel_name: Mapped[str] = mapped_column(Text)
+
+    #Цей рядок не позволяє БД створювати двя рядки з однаковим channel_id
+    __table_args__ = (
+        UniqueConstraint("channel_id"),
+    )
+
+    #Цей рядок дозволяє ORM встановлювати зв'язки між SourceMessage та TargetMessage
+    source_messages: Mapped[list["SourceMessage"]] = relationship(back_populates="channel")
+
+#Таблиця повідомлень каналу донора
+class SourceMessage(Base):
+    __tablename__ = "source_messages"
+
+    #ID рядка в таблиці
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    #ID повідомлення та каналу в якому воно опубліковане
+    channel_id: Mapped[int] = mapped_column(ForeignKey("channels.channel_id"))
+    message_id: Mapped[int] = mapped_column(index=True)
+    
+    #Інформація про повідомлення
+    text: Mapped[str | None] = mapped_column(Text)
+    price: Mapped[int | None] = mapped_column(Integer)
+
+    #Визначаємо чи це повідомлення з товаром та чи воно видалене
+    is_product: Mapped[bool] = mapped_column(Boolean, default=True)
+    is_deleted: Mapped[bool] = mapped_column(Boolean, default=False)
+
+    channel: Mapped["Channels"] = relationship(back_populates="source_messages")
+    target_messages: Mapped[list["TargetMessage"]] = relationship(back_populates="source_message")
+
+    __table_args__ = (
+        UniqueConstraint("channel_id", "message_id"),
+    )
+
+
+class TargetMessage(Base):
+    __tablename__ = "target_messages"
+
+    #ID рядка в таблиці
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    #ID повідомлення в таблиці з яким зв'язане наше повідомлення
+    source_id: Mapped[int] = mapped_column(ForeignKey("source_messages.id"))
+    
+    #ID нашого повідомлення
+    message_id: Mapped[int] = mapped_column(index=True)
+    
+    #Інформація про повідомлення
+    text: Mapped[str | None] = mapped_column(Text)
+    price: Mapped[int | None] = mapped_column(Integer)
+
+    #Статус нашого повідомлення(active, deleted, skipped)
+    status: Mapped[str] = mapped_column(String(20), default="active")
+
+    source_message: Mapped["SourceMessage"] = relationship(back_populates="target_messages")
