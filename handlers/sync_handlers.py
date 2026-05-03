2e101bd0f5c2f97de8ac1c41b656a61ac3dad8bc
Mark <marktishuk@gmail.com>
2026-05-03 17:49:50 +0200

    bring up catch_up_history in main

--- handlers/sync_handlers.py
+++ handlers/sync_handlers.py
@@ -3,12 +3,10 @@ from hydrogram import Client, filters
 from hydrogram.types import Message
 from core.client import userbot, app
 from logic.parser import parse_message, updated_message
-from database.db_handler import save_pair, get_my_id_by_original, check_exists_source_id
-from database.database import SessionLocal
+from database.db_handler import save_pair, get_my_id_by_original, delete_pair
 from config import TARGET_CHANNEL, SOURCE_CHANNEL
 
 
-db = SessionLocal()
 channel=SOURCE_CHANNEL
 
 if(not TARGET_CHANNEL or not SOURCE_CHANNEL):
@@ -33,43 +31,41 @@ async def handle_new_post(client, message):
     
     new_caption = updated_message(parsed, text);
 
-    # 4. Відправляє до каналу нового
-    sent = None
 
-    if message.media_group_id:
-        await asyncio.sleep(2.5)
-        album = await client.get_media_group(message.chat.id, message.id)
-
-        sent = await userbot.safe_send_album(
-            TARGET_CHANNEL,
-            album,
-            new_caption
-        )
-    else:
-        sent = await userbot.safe_copy(
-            TARGET_CHANNEL,
-            message,
-            new_caption
-        )
-
-    # 5. Записує до бази данних (source → target)
-    if sent:
+    # 5. Відправляємо та Записує до бази данних (source → target)
+    if sent := await userbot.send(TARGET_CHANNEL, message, new_caption):
         target_message_id = sent[0].id if isinstance(sent, list) else sent.id
         save_pair(
-            db=db,
             source_id=message.id,
             target_id=target_message_id
         )
 
 @app.on_edited_message(filters.chat(channel))
 async def handle_edit_post(client: Client, message: Message):
-    our_message_id = get_my_id_by_original(db, message.id)
+    our_message_id = get_my_id_by_original(message.id)
     if not our_message_id:
         return
 
-    parsed = parse_message(message.text)
+    text = message.text or message.caption
+    parsed = parse_message(text)
     if not parsed.is_product:
         return
 
-    new_caption = updated_message(parsed, message.text)
-    await userbot.safe_edit(TARGET_CHANNEL, our_message_id, message.text);
+    new_caption = updated_message(parsed, text)
+    await userbot.safe_edit(TARGET_CHANNEL, our_message_id, new_caption);
+
+@app.on_deleted_messages(filters.chat(channel))
+async def handle_delete_post(client: Client, messages: list[Message]):
+
+    for message in messages:
+        our_message_id = get_my_id_by_original(message.id)
+        if not our_message_id:
+            continue
+    
+        text = message.text or message.caption
+        parsed = parse_message(text)
+        if not parsed.is_product:
+            continue
+        await userbot.safe_delete(TARGET_CHANNEL, message.id)
+        delete_pair(message.id)
+
